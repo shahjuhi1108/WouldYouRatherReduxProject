735d1eed8bb9a6d6a2073284fd0b9ea482128724
Juhi <shahjuhi1108@gmail.com>
2020-06-05 00:22:11 -0700

    Updates

---
+++ precache-manifest.e59dacd9ca83fb5c65f270298a3d3a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69acdb53bc46e105fe3af89fe3999037",
+    "url": "/WouldYouRatherReduxProject/index.html"
+  },
+  {
+    "revision": "0557389839f6b8c538ef",
+    "url": "/WouldYouRatherReduxProject/static/css/main.b3fb1ed9.chunk.css"
+  },
+  {
+    "revision": "452e6b1a557bf1fead3b",
+    "url": "/WouldYouRatherReduxProject/static/js/2.6410e1f1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/WouldYouRatherReduxProject/static/js/2.6410e1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0557389839f6b8c538ef",
+    "url": "/WouldYouRatherReduxProject/static/js/main.547bac5a.chunk.js"
+  },
+  {
+    "revision": "ef81d76e6ada975cf3c9",
+    "url": "/WouldYouRatherReduxProject/static/js/runtime-main.5386bc29.js"
+  }
+]);
